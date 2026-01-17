b791e98af3cf6f3dac1a23f1d71f485dabd10ba9
WANG,Youtian <wangyoutian@nilnul.com>
2026-01-17 16:11:31 +0800

    unignore

---
+++ nilnul0/collective_/countable_/IInfinite.cs
@@ -0,0 +1,37 @@
+namespace nilnul.collective_.countable_
+{
+	/// <summary>
+	/// nonfinite collection;
+	/// </summary>
+	/// <remarks>
+	/// cardinality of this is omega[0]
+	/// </remarks>
+	/// alias:
+	///		nonfinite
+	///		denumerable
+	///			,where 'd' is also the initial of <see cref="collective_.ICountable"/>
+	///			dense
+	///			enumerouse
+	///			whileas enumerable means finite, denumerable means nonenumerable.
+	///		denumerous
+	///		deluxe
+	///		
+	///		
+	///		dense
+	///		denormorous
+	///		enumerable
+	///		streamer
+	///		nonending
+	///		numerous
+	///		enumerous
+	///			like enormous
+	///		surnumerous
+	///		
+	///	<see cref="collective_.IDenumerous"/>
+	public interface IInfinite
+		:
+		ICountable
+		,
+		collective_.IInfinite
+	{ }
+}
